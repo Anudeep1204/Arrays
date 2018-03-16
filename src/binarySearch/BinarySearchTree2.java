880812531640904a33d2358917c2bc371f0e276d
sys-51 <sys-51@sys-50-PC>
2018-03-16 09:16:24 +0530

    Arrays

---
+++ src/binarySearch/BinarySearchTree2.java
@@ -0,0 +1,26 @@
+package binarySearch;
+
+public class BinarySearchTree2 {
+	public static void main(String []args) {
+	int a[]= {1,2,3,4,5};
+	int key=11;
+	int low=0;
+	int high=a.length-1;
+	while(low<=high) {
+		int mid=(low+high)/2;
+			if(a[mid]<key) {
+				low=mid+1;
+			}
+			else if(a[mid]>key) {
+				high=mid-1;
+			}
+			else {
+				System.out.println("Key Found "+a[mid]);
+				break;
+			}
+	}
+	if(low>high) {
+		System.out.println("Element "+key+" Not Found!");
+	}
+}
+}
